9c93e67202c5f4a60ae91bf76f0693de36996217
Victor Bernardino <victorbernardinofc@gmail.com>
2026-08-11 21:43:22 -0300

    add localstorage theme

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+const html = document.documentElement;
+const img = document.querySelector("#profile img");
+
+
 function toggleMode() {
-  const html = document.documentElement;
-  html.classList.toggle("light");
-  /*
-    if(html.classList.contains('light')){
-        html.classList.remove('light')
-    } 
-    else {
-      html.classList.add('light')
-    }*/
-  const img = document.querySelector("#profile img");
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png");
-  } else {
-    img.setAttribute('src', "./assets/avatar.png");
-  }
- 
+  const currentTheme = html.classList.contains("light")
+  ? "light"
+  : "dark";
+  
+  const newTheme = currentTheme === "light"
+  ? "dark"
+  : "light";
+  
+  applyTheme(newTheme);
 }
+
+function loadTheme() {
+  const savedTheme = localStorage.getItem("theme") || "light";
+  
+  applyTheme(savedTheme);
+}
+function applyTheme(theme) {
+    if (theme === "dark") {
+        html.classList.remove("light");
+        img.setAttribute("src", "./assets/avatar.png");
+    } else {
+        html.classList.add("light");
+        img.setAttribute("src", "./assets/avatar-light.png");
+    }
+
+    localStorage.setItem("theme", theme);
+}
+
+loadTheme();
